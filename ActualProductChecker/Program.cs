2f85bc58582a7162c272e6b7f77b3d764e36b467
y-watanabe <yoshiyuki-watanabe@koken-kogyo.co.jp>
2023-01-25 09:34:54 +0900

    新規作成 Ver.230116.01

---
+++ ActualProductChecker/Program.cs
@@ -0,0 +1,352 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ActualProductChecker
+{
+    internal class Program
+    {
+        private static bool fales;
+
+        class Target
+        {
+            public Target(string folder) {
+                Folder = folder;
+                CntIn = 0;
+                CntTg = 0;
+                CntOk = 0;
+                CntNg = 0;
+                CntNothing = 0;
+                AttachFiles = new List<string>();
+            }
+
+            public bool IsActivate() {
+                if (CntOk + CntNg > 0) return true;
+                return false;
+            }
+
+            public string Folder { get; set; }
+            public int CntIn { get; set; }
+            public int CntTg { get; set; }
+            public int CntOk { get; set; }
+            public int CntNg { get; set; }
+            public int CntNothing { get; set; }
+            public List<string> AttachFiles { get; set; }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Common.PROGRAM_TITLE + "\n");
+
+            // お膳立て
+            var cmn = new Common();                     // 共通クラス
+            cmn.DbCd = new DBConfigData();              // データベース設定データ
+            cmn.FsCd = new FSConfigData[2];             // 設定データ クラス
+            cmn.SMTPCd = new SMTPConfigData();          // SMTP設定データ クラス
+            cmn.BaseDir = AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\');          // 実行ファイルのあるディレクトリ
+            cmn.DbConfFilePath = Path.Combine(cmn.BaseDir, Common.CONFIG_FILE_DB);      // データベース設定データは実行ファイルと同一ディレクトリ
+            cmn.FsConfFilePath = Path.Combine(cmn.BaseDir, Common.CONFIG_FILE_FS);      // ファイルシステム設定データは実行ファイルと同一ディレクトリ
+            cmn.MailConfFilePath = Path.Combine(cmn.BaseDir, Common.CONFIG_FILE_SMTP);  // SMTP設定データは実行ファイルと同一ディレクトリ
+            cmn.Dba = new DBAccess();                           // データベース アクセス クラス
+            cmn.Fa = new FileAccess();                          // ファイル アクセス クラス
+            cmn.DbCd = cmn.Fa.ReserializeDBConfigFile(cmn);     // データベース設定ファイル逆シリアライズ
+            cmn.FsCd = cmn.Fa.ReserializeFSConfigFile(cmn);     // ファイルシステム設定ファイル逆シリアライズ
+            cmn.SMTPCd = cmn.Fa.ReserializeMailConfigFile(cmn); // メール送信者情報設定ファイル逆シリアライズ
+            OracleConnection cnn = null;
+            if (cmn.Dba.ConnectSchema(cmn, ref cnn))            // データベース接続
+            {
+                cmn.Cnn = cnn;
+            } else { 
+                Console.WriteLine("データベースに接続できませんでした．\nメール送信なしでファイルのチェックだけを行いますか？ (y/n)");
+                if (Console.ReadKey().Key.ToString().ToLower() != "y") return;
+                Console.WriteLine();
+                cmn.Cnn = null;
+            }
+
+
+
+            // データベース関連情報取得
+            var myDs = new DataSet();
+            var myIRepoList = new List<string>();
+            if (cmn.Cnn != null)
+            {
+                // データベースから送信先アドレスを取得
+                if (!cmn.Dba.GetEmailAddress(cmn, Common.EMAIL_ADRTYPE_TO, ref myDs)) return;
+                if (!cmn.Dba.GetEmailAddress(cmn, Common.EMAIL_ADRTYPE_CC, ref myDs)) return;
+                // データベースから有効かつ運用中の工程名称を取得
+                if (!cmn.Dba.GetIRepoKt(cmn, Common.A70_ACTIVE_ACTIVE, Common.A70_OPESTAT_OPARATED, ref myDs)) return;
+                myIRepoList = myDs.Tables[Common.TABLE_NAME_A70].AsEnumerable()
+                    .Select(row => row.Field<string>("IREPOKTNM")).ToList();
+            }
+
+
+
+            // サーバーのファイルシステムに接続
+            var sourceFolder = @cmn.FsCd[Common.CONNECT_SOURCE].RootPath;
+            var backupFolder = @cmn.FsCd[Common.CONNECT_DEST].RootPath + "\\" + Common.DI_BACKUP;
+            var errorFolder = @cmn.FsCd[Common.CONNECT_DEST].RootPath + "\\" + Common.DI_ERROR;
+            if (!cmn.Fa.ConnectServer(cmn, Common.CONNECT_SOURCE)) return;
+            if (!cmn.Fa.ConnectServer(cmn, Common.CONNECT_DEST)) return;
+            if (!Directory.Exists(backupFolder))
+            {
+#if DEBUG
+                Console.WriteLine("自動受入バックアップ先フォルダーがみつかりませんでした．");
+                Console.ReadKey();
+#endif
+                WriteLog("自動受入バックアップ先フォルダーがみつかりませんでした．");
+                return;
+            }
+            if (!Directory.Exists(errorFolder))
+            {
+#if DEBUG
+                Console.WriteLine("自動受入エラーフォルダーがみつかりませんでした．");
+                Console.ReadKey();
+#endif
+                WriteLog("自動受入エラーフォルダーがみつかりませんでした．");
+                return;
+            }
+
+
+
+            // [KMC006SC] 製造実績ファイル照合処理開始
+            Console.WriteLine();
+            var Targets = new List<Target>();
+            if (args.Length == 0) //当日処理
+            {
+                var TargetDt = DateTime.Today; // DateTime.Parse("2020/9/24"); // DateTime.Today; //.AddDays(-1);  // 対象となる処理日付
+                // 判定対象フォルダの存在チェック
+                if (!Directory.Exists(sourceFolder + "\\" + TargetDt.ToString("yyyyMMdd")))
+                {
+                    WriteLog("本日の実績登録はありませんでした．");
+                }
+                else
+                {
+                    Targets.Add(new Target(TargetDt.ToString("yyyyMMdd")));
+                    ActualProductChecker(cmn, myIRepoList, Targets[0]);
+                }
+            }
+            else
+            {
+                if (args[0] == "/all")
+                {
+                    DirectoryInfo di = new DirectoryInfo(sourceFolder);
+                    DirectoryInfo[] diAlls = di.GetDirectories();
+                    foreach (DirectoryInfo d in diAlls)
+                    {
+                        Targets.Add(new Target(d.Name));
+                        ActualProductChecker(cmn, myIRepoList, Targets[Targets.Count() - 1]);
+                    }
+                }
+            }
+            
+            // 結果をメール送信
+            var cIn = Targets.Select(i => i.CntIn).Sum();
+            var cTg = Targets.Where(row => row.IsActivate()).Select(col => col.CntTg).Sum();
+            var cOk = Targets.Where(row => row.IsActivate()).Select(col => col.CntOk).Sum();
+            var cNg = Targets.Where(row => row.IsActivate()).Select(col => col.CntNg).Sum();
+            var cNothing = Targets.Where(row => row.IsActivate()).Select(col => col.CntNothing).Sum();
+            if (cmn.Cnn != null)
+            {
+                try
+                {
+                    MailMessage msg = new MailMessage();
+                    msg.From = new MailAddress(cmn.SMTPCd.From); // 送信元("コーケン工業　システム管理者 <yoshiyuki-watanabe@koken-kogyo.co.jp>")
+                    foreach (DataRow result in myDs.Tables["To"].Rows)
+                    {
+                        msg.To.Add(new MailAddress(result["EMAIL"].ToString()));
+                    }
+                    foreach (DataRow result in myDs.Tables["Cc"].Rows)
+                    {
+                        msg.CC.Add(new MailAddress(result["EMAIL"].ToString()));
+                    }
+                    msg.Subject = Common.MAIL_SUBJECT;
+                    if (cIn == 0)
+                    {
+                        msg.Body = Common.MAIL_BODY_HEADER + 
+                            "実績登録はありませんでした．\n\n" +
+                            Common.MAIL_BODY_FOOTER;
+                    }
+                    else
+                    {
+                        msg.Body = Common.MAIL_BODY_HEADER +
+                            "入力完了帳票：" + cIn + "件\n" +
+                            "　受入対象：" + cTg + "件\n" +
+                            "　自動受入成功：" + cOk + "件\n" +
+                            "　自動受入失敗：" + cNg + "件\n\n" +
+                            "　行方不明となったファイル：" + cNothing + "件\n" +
+                            Common.MAIL_BODY_FOOTER;
+                        // 自動受入ログの添付（実行中はファイルにアクセス出来ない為複写して添付）
+                        if (File.Exists(Common.JIDOU_LOG_PATH + "\\" + Common.JIDOU_LOG_FILE))
+                        {
+                            var jidoulog = @cmn.BaseDir + "\\" + Common.JIDOU_LOG_FILE;
+                            File.Copy(@Common.JIDOU_LOG_PATH + "\\" + Common.JIDOU_LOG_FILE, jidoulog, true);
+                            Attachment attachjidou = new Attachment(jidoulog);
+                            msg.Attachments.Add(attachjidou);
+                        }
+
+                        // 失敗もしくは不明があればファイルを添付する
+                        var attachfiles = Targets
+                            .Where(t => t.IsActivate() && t.AttachFiles.Count > 0)
+                            .SelectMany(a => a.AttachFiles);
+                        // 対象が10件以上ある場合はテキストファイルに内容を作成して添付する
+                        var listflg = (attachfiles.ToList().Count > 10);
+                        var errorlist = "";
+                        foreach (var fl in attachfiles)
+                        {
+                            if (listflg)
+                            {
+                                errorlist += fl + "\n";
+                            }
+                            else
+                            {
+                                var attachment = new System.Net.Mail.Attachment(fl);
+                                msg.Attachments.Add(attachment);
+                            }
+                        }
+                        if (errorlist != "")
+                        {
+                            WriteList(errorlist);
+                            System.Net.Mail.Attachment attachment;
+                            attachment = new System.Net.Mail.Attachment(
+                                @Path.Combine(Directory.GetCurrentDirectory(), Common.ERROR_FILE_LIST));
+                            msg.Attachments.Add(attachment);
+                        }
+                    }
+
+                    SmtpClient sc = new SmtpClient();
+
+                    sc.EnableSsl = true;
+                    sc.UseDefaultCredentials = false;
+                    sc.Credentials = new System.Net.NetworkCredential(cmn.SMTPCd.LoginUser, cmn.SMTPCd.LoginPass); //uX[HgKn{djM56=xc
+                    sc.Host = cmn.SMTPCd.Server; // "140.227.104.69";
+                    sc.Port = cmn.SMTPCd.Port; // 587;
+                    sc.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                    // SSL証明書チェックのコールバック設定
+                    ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(ValidateServerCertificate);
+
+                    // メール送信
+                    sc.Send(msg);
+
+                    // データベースクローズ
+                    cmn.Dba.CloseSchema(cnn);
+
+                    // 結果ログ出力
+                    if (cIn != 0)
+                    {
+                        var tg = String.Format("{0,3}", cTg);
+                        var ok = String.Format("{0,3}", cOk);
+                        var mode = args.Length == 0 ? "" : "全件チェック ";
+                        WriteLog(mode + $"自動受入 対象:{tg}件 / 正常:{ok}件 / 失敗:{cNg}件 / 不明:{cNothing}件");
+                    }
+
+                    Console.WriteLine("Send Mail Compleated !!");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        public static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true; // .NET Frameworkに対して「SSL証明書の使用は問題なし」
+            else
+            {
+                // false の場合は「SSL証明書の使用は不可」
+                // 何かしらのチェックを行いtrue / false を判定
+                // このプログラムでは true を返却し、信頼されないSSL証明書の問題を回避
+                return true; 
+            }
+        }
+
+        private static void WriteLog(string message)
+        {
+            var mySW = new StreamWriter(
+                @Path.Combine(Directory.GetCurrentDirectory(), Common.LOG_FILE)
+                , true
+                , System.Text.Encoding.GetEncoding("shift_jis"));
+            mySW.WriteLine("[" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + "] " + message);
+            mySW.Close();
+        }
+
+        private static void WriteList(string message)
+        {
+            var mySW = new StreamWriter(
+                @Path.Combine(Directory.GetCurrentDirectory(), Common.ERROR_FILE_LIST)
+                , false
+                , System.Text.Encoding.GetEncoding("shift_jis"));
+            mySW.WriteLine(message);
+            mySW.Close();
+        }
+
+        private static bool IRepoFileExists(string dirname, string filename)
+        {
+            var files = Directory.GetFiles(dirname, filename.Replace(".csv", "*.csv"));
+            if (files.Length == 0) { return false; }
+            return true;
+        }
+
+        private static void ActualProductChecker(Common cmn, List<string> irepolist, Target tg)
+        {
+            var srcFolder = @cmn.FsCd[Common.CONNECT_SOURCE].RootPath + "\\" + tg.Folder;
+            var backupFolder = @cmn.FsCd[Common.CONNECT_DEST].RootPath + "\\" + Common.DI_BACKUP;
+            var errorFolder = @cmn.FsCd[Common.CONNECT_DEST].RootPath + "\\" + Common.DI_ERROR;
+
+            // SOURCEディレクトリ直下の当日フォルダをフィルター付きで調査
+            //Console.WriteLine("[" + srcFolder + "] ディレクトリ内のファイルを調査します");
+            var message = "[" + srcFolder + "] ディレクトリ内のファイルを調査します\n";
+            var dis = new DirectoryInfo(srcFolder);
+            var fiAlls = dis.GetFiles(cmn.FsCd[Common.CONNECT_SOURCE].FileFilter);
+            foreach (FileInfo fis in fiAlls)
+            {
+                tg.CntIn++;
+                if (cmn.Cnn == null || irepolist.Contains(fis.Name.Split('_')[0]))
+                {
+                    tg.CntTg++;
+                    message += "  " + fis.Name;
+                    if (IRepoFileExists(backupFolder, fis.Name))
+                    {
+                        tg.CntOk++;                             // backupフォルダに存在
+                        message += " -- > ok\n";
+                    }
+                    else
+                    {
+                        if (IRepoFileExists(errorFolder, fis.Name))
+                        {
+                            tg.CntNg++;                         // errorフォルダに存在
+                            tg.AttachFiles.Add(fis.FullName);
+                            message += " 登録エラー\n";
+                        }
+                        else
+                        {
+                            tg.CntNothing++;                    // 転送失敗
+                            tg.AttachFiles.Add(fis.FullName);
+                            message += " 転送失敗！ファイルが見つかりませんでした．\n";
+                        }
+
+                    }
+                }
+            }
+            if (tg.CntOk + tg.CntNg == 0)
+            {
+                tg.CntNothing = 0;
+                tg.AttachFiles = null;
+            }
+            else
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,92 +1,57 @@
-# ActualProductChecker
+# ActualProductChecker  
 
-[KMC006SC] 製造実績ファイル照合
+[KMC006SC] 製造実績ファイル照合  
 
-## Getting started
 
-To make it easy for you to get started with GitLab, here's a list of recommended next steps.
+## 概要  
 
-Already a pro? Just edit this README.md and make it your own. Want to make it easy? [Use the template at the bottom](#editing-this-readme)!
+・ireposv サーバーの 入力完了帳票 と kemsvr2 サーバーの 自動受入 されたファイルを比較照合。  
+・比較照合結果をメールにて配信する。  
+・配信の際、kemsvr2 サーバーの 自動受入ログファイル と 受入エラーとなったファイルを添付。  
 
-## Add your files
 
-- [ ] [Create](https://docs.gitlab.com/ee/user/project/repository/web_editor.html#create-a-file) or [upload](https://docs.gitlab.com/ee/user/project/repository/web_editor.html#upload-a-file) files
-- [ ] [Add files using the command line](https://docs.gitlab.com/ee/gitlab-basics/add-file.html#add-a-file-using-the-command-line) or push an existing Git repository with the following command:
+## 開発環境  
 
-```
-cd existing_repo
-git remote add origin http://192.168.96.198/koken/ActualProductChecker.git
-git branch -M main
-git push -uf origin main
-```
+Visual Studio Professional 2022 (Visual C# 2022)  
 
-## Integrate with your tools
 
-- [ ] [Set up project integrations](http://192.168.96.198/koken/ActualProductChecker/-/settings/integrations)
+## アプリケーションの種類  
 
-## Collaborate with your team
+コンソール アプリ (.NET Framework) C#  
 
-- [ ] [Invite team members and collaborators](https://docs.gitlab.com/ee/user/project/members/)
-- [ ] [Create a new merge request](https://docs.gitlab.com/ee/user/project/merge_requests/creating_merge_requests.html)
-- [ ] [Automatically close issues from merge requests](https://docs.gitlab.com/ee/user/project/issues/managing_issues.html#closing-issues-automatically)
-- [ ] [Enable merge request approvals](https://docs.gitlab.com/ee/user/project/merge_requests/approvals/)
-- [ ] [Automatically merge when pipeline succeeds](https://docs.gitlab.com/ee/user/project/merge_requests/merge_when_pipeline_succeeds.html)
 
-## Test and Deploy
+## メンバー  
 
-Use the built-in continuous integration in GitLab.
+y.watanabae  
 
-- [ ] [Get started with GitLab CI/CD](https://docs.gitlab.com/ee/ci/quick_start/index.html)
-- [ ] [Analyze your code for known vulnerabilities with Static Application Security Testing(SAST)](https://docs.gitlab.com/ee/user/application_security/sast/)
-- [ ] [Deploy to Kubernetes, Amazon EC2, or Amazon ECS using Auto Deploy](https://docs.gitlab.com/ee/topics/autodevops/requirements.html)
-- [ ] [Use pull-based deployments for improved Kubernetes management](https://docs.gitlab.com/ee/user/clusters/agent/)
-- [ ] [Set up protected environments](https://docs.gitlab.com/ee/ci/environments/protected_environments.html)
 
-***
+## プロジェクト構成  
 
-# Editing this README
+/  
+├ActualProductChecker/  
+｜├Common.cs　　　# 共通 クラス  
+｜├DBAccess.cs　　　# データベースアクセ スクラス  
+｜├FileAccess.cs　　　# ファイルアクセス クラス  
+｜└Program.cs　　　# コンソール アプリケーション本体  
+｜
+├packages/  
+｜├DecryptPassword.dll　　　# パスワード復号化モジュール  
+｜└Oracle.ManagedDataAccess.dll　　　# Oracle接続モジュール  
+｜
+├settingfiles/  
+｜├ConfigDB - KOKEN_1.xml　　　# データベース設定ファイル  
+｜├ConfigDB - KOKEN_5.xml　　　# データベース設定ファイル (テスト用)  
+｜├ConfigFS - kemsvr2.xml　　　# サーバー設定ファイル  
+｜├ConfigFS - pc090n.xml　　　# サーバー設定ファイル (テスト用)  
+｜├ConfigSMTP - gmail.xml　　　# データベース設定ファイル (テスト用)  
+｜└ConfigSMTP - Koken.xml　　　# データベース設定ファイル  
+｜
+├ActualProductChecker.sln　　　# プロジェクトファイル  
+└README.md　　　# このファイル  
 
-When you're ready to make this README your own, just edit this file and use the handy template below (or feel free to structure it however you want - this is just a starting point!). Thank you to [makeareadme.com](https://www.makeareadme.com/) for this template.
 
-## Suggestions for a good README
-Every project is different, so consider which of these sections apply to yours. The sections used in the template are suggestions for most open source projects. Also keep in mind that while a README can be too long and detailed, too long is better than too short. If you think your README is too long, consider utilizing another form of documentation rather than cutting out information.
+## データベース参照テーブル  
 
-## Name
-Choose a self-explaining name for your project.
+A70		i-Reporter 対象工程マスタ  
+KM0040	アドレス帳マスタ  
 
-## Description
-Let people know what your project can do specifically. Provide context and add a link to any reference visitors might be unfamiliar with. A list of Features or a Background subsection can also be added here. If there are alternatives to your project, this is a good place to list differentiating factors.
-
-## Badges
-On some READMEs, you may see small images that convey metadata, such as whether or not all the tests are passing for the project. You can use Shields to add some to your README. Many services also have instructions for adding a badge.
-
-## Visuals
-Depending on what you are making, it can be a good idea to include screenshots or even a video (you'll frequently see GIFs rather than actual videos). Tools like ttygif can help, but check out Asciinema for a more sophisticated method.
-
-## Installation
-Within a particular ecosystem, there may be a common way of installing things, such as using Yarn, NuGet, or Homebrew. However, consider the possibility that whoever is reading your README is a novice and would like more guidance. Listing specific steps helps remove ambiguity and gets people to using your project as quickly as possible. If it only runs in a specific context like a particular programming language version or operating system or has dependencies that have to be installed manually, also add a Requirements subsection.
-
-## Usage
-Use examples liberally, and show the expected output if you can. It's helpful to have inline the smallest example of usage that you can demonstrate, while providing links to more sophisticated examples if they are too long to reasonably include in the README.
-
-## Support
-Tell people where they can go to for help. It can be any combination of an issue tracker, a chat room, an email address, etc.
-
-## Roadmap
-If you have ideas for releases in the future, it is a good idea to list them in the README.
-
-## Contributing
-State if you are open to contributions and what your requirements are for accepting them.
-
-For people who want to make changes to your project, it's helpful to have some documentation on how to get started. Perhaps there is a script that they should run or some environment variables that they need to set. Make these steps explicit. These instructions could also be useful to your future self.
-
-You can also document commands to lint the code or run tests. These steps help to ensure high code quality and reduce the likelihood that the changes inadvertently break something. Having instructions for running tests is especially helpful if it requires external setup, such as starting a Selenium server for testing in a browser.
-
-## Authors and acknowledgment
-Show your appreciation to those who have contributed to the project.
-
-## License
-For open source projects, say how it is licensed.
-
-## Project status
-If you have run out of energy or time for your project, put a note at the top of the README saying that development has slowed down or stopped completely. Someone may choose to fork your project or volunteer to step in as a maintainer or owner, allowing your project to keep going. You can also make an explicit request for maintainers.
